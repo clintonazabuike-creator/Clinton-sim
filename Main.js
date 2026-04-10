aa456193dd5a86e5aedbba3c5bbaaef6b6eae27e
clintonazabuike-creator <clintonazabuike@gmail.com>
2026-04-10 05:04:36 +0100

    Main.js

--- Main.js
+++ Main.js
@@ -1,33 +1,15 @@
-// js/Main.js
-import { World } from './World.js';
-import { Entity } from './Entities.js';
-
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const world = new World(Math.ceil(canvas.width / 32), Math.ceil(canvas.height / 32), 32);
-const entities = [];
-
-// Spawn initial population
-for (let i = 0; i < 10; i++) {
-    entities.push(new Entity(Math.random() * canvas.width, Math.random() * canvas.height, 'human'));
-}
+// js/Main.js Update
 
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // 1. Render World
     world.render(ctx);
 
-    // 2. Update and Render Entities
     entities.forEach(ent => {
-        ent.update(world);
+        // Pass 'entities' list so they can hunt or run
+        ent.update(world, entities);
         ent.draw(ctx);
     });
-
+    
+    if (ui.selectedEntity) ui.updateCard();
     requestAnimationFrame(gameLoop);
 }
-
-gameLoop();
